476af949f96f2fbcba6e397a74bd7859f39481fb
claudiaGiancola <cloemezzaluna@gmail.com>
2025-04-29 21:28:46 +0100

    tabs default setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import { ButtonDefault } from './components/button';
 import { StepperWithContent } from './components/stepper';
+import { TabsDefault } from './components/tabs';
 
 function App() {
 
   return (
     <>
+      <TabsDefault />
       <StepperWithContent />
     </>
   )
