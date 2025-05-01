9e8f31e8fb70d7287cd678430fedc57c0cd5ef1a
claudiaGiancola <cloemezzaluna@gmail.com>
2025-05-01 01:00:02 +0100

    card finale

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
-import { CarouselCustomNavigation, CarouselWithContent } from './components/carousel';
-import CollapseDefault from './components/collapse';
-import { DialogWithBillingForm } from './components/dialog';
+
 import { SimpleFooter } from './components/footer';
-import { ImgWithBlurredCaption } from './components/image';
+import { ImgWithBlurredCaption, ImgWithText } from './components/image';
 import { StickyNavbar } from './components/navbar';
 import { TabsDefault } from './components/tabs';
 import { TimelineWithIcon } from './components/timeline';
@@ -23,14 +21,8 @@ function App() {
       </div>
 
         {/* <TabsDefault /> */}
-
-        <CollapseDefault />
-
-        <DialogWithBillingForm />
-
-        <CarouselCustomNavigation />
-        <CarouselWithContent />
         
+        <ImgWithText />
 
       <SimpleFooter />
     </>
--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,4 +1,9 @@
 import Countdown from "./countdown";
+import {
+    Typography,
+    Button,
+} from "@material-tailwind/react";
+import { DialogDefault, DialogWithBillingForm, ModalWithImage } from "./dialog";
 
 export function ImgWithBlurredCaption() {
     return (
@@ -15,5 +20,46 @@ export function ImgWithBlurredCaption() {
                 </div>
             </figcaption>
         </figure>
+    );
+}
+
+export function ImgWithText() {
+    return (
+        <div className="relative h-full w-full">
+            <img
+                src="src\assets\Hokusai cherry blossoms.jpeg"
+                alt="image 2"
+                className="h-full w-full object-cover"
+            />
+            <div className="absolute inset-0 grid h-full w-full items-center">
+                <div className="px-12">
+                    <Typography
+                        variant="h1"
+                        color="white"
+                        className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+                    >
+                        The Beauty of Nature
+                    </Typography>
+                    <Typography
+                        variant="lead"
+                        color="white"
+                        className="mb-12 opacity-80"
+                    >
+                        It is not so much for its beauty that the forest makes a claim upon men.
+                    </Typography>
+                    <div className="flex gap-2">
+                        <Button size="lg" color="white">
+                            Explore
+                        </Button>
+                        <Button size="lg" color="white" variant="text">
+                            Gallery
+                        </Button>
+                        <DialogWithBillingForm />
+                        <DialogDefault />
+                        <ModalWithImage />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
