79498b80c5f33399598e39d375462458ab4c0cf3
claudiaGiancola <cloemezzaluna@gmail.com>
2025-06-18 15:40:19 +0100

    added google maps on timeline

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,36 +1,37 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import 'leaflet/dist/leaflet.css';
+// import L from 'leaflet';
 
-export default function MapWithMarker({ position, popupText, markerColour }) {
+export default function MapWithMarker({ srcgooglemap }) {
 
-    const colouredMarker = new L.Icon({
-        iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${markerColour}.png`,
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
+    // const colouredMarker = new L.Icon({
+    //     iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${markerColour}.png`,
+    //     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    //     iconSize: [25, 41],
+    //     iconAnchor: [12, 41],
+    //     popupAnchor: [1, -34],
+    //     shadowSize: [41, 41]
+    // });
+console.log("srcgooglemap:", srcgooglemap);
 
     return (
-        <div className="w-60 sm:w-96 h-52 sm:h-64 rounded-lg overflow-hidden">
-            <MapContainer
-                center={position}
-                zoom={13}
-                scrollWheelZoom={true}
-                style={{ height: '100%', width: '100%' }}
-            >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position} icon={colouredMarker}>
-                    <Popup>
-                        {popupText}
-                    </Popup>
-                </Marker>
-            </MapContainer>
+        <div 
+        // className="w-60 sm:w-96 h-52 sm:h-64 rounded-lg overflow-hidden"
+        >
+            <iframe
+                title="Google Maps"
+                src={srcgooglemap}
+                width="800"
+                height="600"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+
+
+
         </div>
     )
 }
+
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -33,7 +33,7 @@ import MapWithMarker from './map';
 export function TimelineWithIcon() {
   return (
     <div className="px-8">
-      <Timeline>
+      <Timeline className="">
         <TimelineItem>
           <TimelineConnector />
           <TimelineHeader>
@@ -44,14 +44,15 @@ export function TimelineWithIcon() {
               6 Settembre | 16:30 | Cerimonia
             </Typography>
           </TimelineHeader>
-          <TimelineBody className="pb-8">
+          <TimelineBody className="pb-8 flex flex-col items-center">
             {/* <Typography color="gary" className="font-normal text-gray-600">
               The key to more success is to have a lot of pillows.
             </Typography> */}
             <MapWithMarker
-              position={[42.15874995882924, 12.224626909233246]}
-              popupText="CERIMONIA: Via della Rena, 99, 00069 Trevignano Romano RM, Italy"
-              markerColour="green"
+              // position={[42.15874995882924, 12.224626909233246]}
+              // popupText="CERIMONIA: Via della Rena, 99, 00069 Trevignano Romano RM, Italy"
+              // markerColour="green"
+              srcgooglemap="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5915.209887606379!2d12.22462!3d42.15874!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f48ee4325b191%3A0x5160683cbff8a03!2sVia%20della%20Rena%2C%2099%2C%2000069%20Trevignano%20Romano%20RM%2C%20Italia!5e0!3m2!1sit!2suk!4v1750257112136!5m2!1sit!2suk"
             />
           </TimelineBody>
         </TimelineItem>
@@ -65,14 +66,15 @@ export function TimelineWithIcon() {
               6 Settembre | 18:00 | Ricevimento
             </Typography>
           </TimelineHeader>
-          <TimelineBody className="pb-8">
+          <TimelineBody className="pb-8 flex flex-col items-center">
             {/* <Typography color="gary" className="font-normal text-gray-600">
               The key to more success is to have a lot of pillows.
             </Typography> */}
             <MapWithMarker
-              position={[42.16498943948103, 12.229883605276306]}
-              popupText="RICEVIMENTO: Il Frantoio, Via Sutri, 3, 00069 Trevignano Romano RM, Italy"
-              markerColour="gold"
+              // position={[42.16498943948103, 12.229883605276306]}
+              // popupText="RICEVIMENTO: Il Frantoio, Via Sutri, 3, 00069 Trevignano Romano RM, Italy"
+              // markerColour="gold"
+              srcgooglemap="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2957.313125639062!2d12.227272276909249!3d42.16498327121171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f48f37bd8c223%3A0x118ccff4c129e3fa!2sIl%20Frantoio!5e0!3m2!1sit!2suk!4v1750254856932!5m2!1sit!2suk"
             />
           </TimelineBody>
         </TimelineItem>
@@ -104,13 +106,14 @@ export function TimelineWithIcon() {
           </TimelineHeader>
           <TimelineBody className="pb-8">
             <div className="flex flex-col gap-2">
-              <Typography color="gray" className="text-gray-600 text-left w-[60%]">
+              <Typography color="gray" className="text-gray-600 text-left sm:w-[60%]">
                 Per chi ha ancora energie e voglia di festeggiare (e magari fare un bagno al lago)!
               </Typography>
               <MapWithMarker
-                position={[42.15874995882924, 12.224626909233246]}
-                popupText="CERIMONIA: Via della Rena, 99, 00069 Trevignano Romano RM, Italy"
-                markerColour="green"
+                // position={[42.15874995882924, 12.224626909233246]}
+                // popupText="CERIMONIA: Via della Rena, 99, 00069 Trevignano Romano RM, Italy"
+                // markerColour="green"
+                srcgooglemap="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5915.209887606379!2d12.22462!3d42.15874!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f48ee4325b191%3A0x5160683cbff8a03!2sVia%20della%20Rena%2C%2099%2C%2000069%20Trevignano%20Romano%20RM%2C%20Italia!5e0!3m2!1sit!2suk!4v1750257112136!5m2!1sit!2suk"
               />
             </div>
           </TimelineBody>
