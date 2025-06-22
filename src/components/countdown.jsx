7937170e5956743ffb837fedfa3fc5c6fc25ec3d
claudiaGiancola <cloemezzaluna@gmail.com>
2025-06-22 16:57:39 +0100

    Fix countdown position

--- src/components/countdown.jsx
+++ src/components/countdown.jsx
@@ -5,13 +5,13 @@ import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
 export default function Countdown() {
     return (
-        <div className="countdown-container">
+        <div className='pb-[11vh]'>
             <FlipClockCountdown
                 to={new Date("2025-09-06T16:30:00").getTime() + 24 * 3600 * 1000 + 5000}
                 className="countdown"
                 labels={['Days', 'Hours', 'Minutes', 'Seconds']}
-                labelStyle={{ fontSize: 20, color: '#363430'}}
-                digitBlockStyle={{ width: 30, height: 55, fontSize: 40, background: '#f6b092', borderRadius: 8, boxShadow: '0 6px 8px rgba(0, 0, 0, 0.4)' }}
+                labelStyle={{ fontSize: 16, color: '#363430'}}
+                digitBlockStyle={{ width: 30, height: 50, fontSize: 35, background: '#f6b092', borderRadius: 8, boxShadow: '0 6px 8px rgba(0, 0, 0, 0.4)' }}
             />
         </div>
     );
