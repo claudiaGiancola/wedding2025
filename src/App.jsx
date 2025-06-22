4e9c37f7ae2449ba789bb874fda3b6ef5f428d70
claudiaGiancola <cloemezzaluna@gmail.com>
2025-06-22 16:34:44 +0100

    TEST parallasse css only

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
 import './App.css'
 import { Typography } from "@material-tailwind/react";
 import { SimpleFooter } from './components/footer';
-import { ImgWithText } from './components/image'
 import { StickyNavbar } from './components/navbar';
-import { HeroParallax, ParallaxReveal } from './components/parallax';
 import { TimelineWithIcon } from './components/timeline';
 import { GiftButtons } from './components/giftButtons';
+
+import invitoMatrimonio from './assets/Invito Matrimonio cropped.jpeg';
+import invitoMatrimonioClaudia from './assets/Invito Matrimonio regalo modified - Claudia.png';
+import invitoMatrimonioGiada from './assets/Invito Matrimonio regalo modified - Giada.png';
+
+import Countdown from './components/countdown';
 
 function App() {
 
   return (
 
-    <div>
+    <div className='wrapper'>
 
       <StickyNavbar />
 
-      <section id="top-illustration" className="gap-0 scroll-mt-20">
-        <HeroParallax />
+      <section id="top-illustration" className="gap-0 scroll-mt-20 header-section">
+
+        <Countdown />
+
+        <img src={invitoMatrimonio} className='background'/>
+
+        {/* <img src={invitoMatrimonioClaudia} className='foreground foreground-L hover:translate-x-[-50vw] transition-transform duration-600'/>
+        <img src={invitoMatrimonioGiada} className='foreground foreground-R'/> */}
+
       </section>
 
-      <div className="relative z-0 flex flex-col items-center justify-center">
+      <div className="relative z-0 flex flex-col items-center justify-center bg-white">
 
         <section id="timeline" className="gap-0 scroll-mt-20 mt-8 w-full">
 
@@ -27,7 +38,7 @@ function App() {
             Timeline
           </Typography>
 
-          <TimelineWithIcon/>
+          <TimelineWithIcon />
         </section>
 
         <section id="regalo" className="gap-0 mt-12 scroll-mt-20 mb-6 sm:mb-8 w-full">
@@ -35,11 +46,9 @@ function App() {
             Regalaci...
           </Typography>
         </section>
-          <GiftButtons />
+        <GiftButtons />
 
       </div>
-
-      <ParallaxReveal />
 
       <SimpleFooter />
 
--- src/components/countdown.jsx
+++ src/components/countdown.jsx
@@ -5,7 +5,7 @@ import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
 export default function Countdown() {
     return (
-        <div className="countdown-container pt-2">
+        <div className="countdown-container">
             <FlipClockCountdown
                 to={new Date("2025-09-06T16:30:00").getTime() + 24 * 3600 * 1000 + 5000}
                 className="countdown"
